7624f919c279624391d078aef3084a867fbebef7
wendson13 <wendson.dev@gmail.com>
2023-01-27 17:43:28 -0300

    feat(initial): create project structure

---
+++ src/styles/global.ts
@@ -0,0 +1,13 @@
+import { createGlobalStyle } from "styled-components";
+
+export const GlobalStyle = createGlobalStyle`
+  *{
+    padding: 0;
+    margin: 0;
+    box-sizing: border-box;
+  }
+  body {
+    width: 100%;
+    height: 100vh;
+  }
+`;
