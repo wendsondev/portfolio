563951b0049fe478f02111c66686890fdfca15d4
wendson13 <wendson.dev@gmail.com>
2023-01-28 00:12:03 -0300

    style(font): add roboto default font

--- src/components/Layout/index.tsx
+++ src/components/Layout/index.tsx
@@ -1,6 +1,9 @@
 import { ContextThemeProvider } from '@contexts/Theme';
+import { Roboto } from '@next/font/google';
 import { GlobalStyle } from '@styles/global';
 import Head from 'next/head';
+
+const roboto = Roboto({ subsets: ['latin'], weight: ['400', '700', '900'] });
 
 type LayoutProps = {
   children: React.ReactNode;
@@ -8,74 +11,79 @@ type LayoutProps = {
 
 export function Layout({ children }: LayoutProps) {
   return (
-    <ContextThemeProvider>
-      <Head>
-        <meta name="viewport" content="width=device-width, initial-scale=1.0" />
-        <link
-          rel="alternate"
-          href="https://www.wendson.com.br/en"
-          hrefLang="en"
-        />
-        <link
-          rel="alternate"
-          href="https://www.wendson.com.br/pt-br"
-          hrefLang="pt-br"
-        />
-        <link
-          rel="alternate"
-          href="https://www.wendson.com.br/en"
-          hrefLang="x-default"
-        />
-        <link rel="shortcut icon" href="favicon.png" type="image/png" />
-        <meta name="title" content="Wendson Sousa - Front-end Developer" />
-        <meta
-          name="description"
-          content="Front-end Developer specialized in React JS and React Native with typescript
-"
-        />
-        <meta property="og:type" content="website" />
-        <meta property="og:url" content="https://www.wendson.com.br/" />
-        <meta
-          property="og:title"
-          content="Wendson Sousa - Front-end Developer ReactJS"
-        />
-        <meta
-          property="og:description"
-          content="Front-end Developer specialized in React JS and React Native with typescript"
-        />
-        <meta
-          property="og:image"
-          content="https://www.wendson.com.br/logo.png"
-        />
-        <meta
-          property="og:image:secure_url"
-          itemProp="image"
-          content="https://www.wendson.com.br/logo.png"
-        />
-        <meta property="og:image:width" content="93" />
-        <meta property="og:image:height" content="48" />
-        <meta property="twitter:card" content="summary_large_image" />
-        <meta property="twitter:url" content="https://www.wendson.com.br/" />
-        <meta
-          property="twitter:title"
-          content="Wendson Sousa - Front-end Developer"
-        />
-        <meta
-          property="twitter:description"
-          content="Front-end Developer specialized in React JS and React Native with typescript"
-        />
-        <meta
-          property="twitter:image"
-          content="https://www.wendson.com.br/logo.png"
-        />
-        ;
-        <meta property="twitter:image:width" content="93" />
-        <meta property="twitter:image:height" content="48" />
-      </Head>
+    <div className={roboto.className}>
+      <ContextThemeProvider>
+        <Head>
+          <meta
+            name="viewport"
+            content="width=device-width, initial-scale=1.0"
+          />
+          <link
+            rel="alternate"
+            href="https://www.wendson.com.br/en"
+            hrefLang="en"
+          />
+          <link
+            rel="alternate"
+            href="https://www.wendson.com.br/pt-br"
+            hrefLang="pt-br"
+          />
+          <link
+            rel="alternate"
+            href="https://www.wendson.com.br/en"
+            hrefLang="x-default"
+          />
+          <link rel="shortcut icon" href="favicon.png" type="image/png" />
+          <meta name="title" content="Wendson Sousa - Front-end Developer" />
+          <meta
+            name="description"
+            content="Front-end Developer specialized in React JS and React Native with typescript
+  "
+          />
+          <meta property="og:type" content="website" />
+          <meta property="og:url" content="https://www.wendson.com.br/" />
+          <meta
+            property="og:title"
+            content="Wendson Sousa - Front-end Developer ReactJS"
+          />
+          <meta
+            property="og:description"
+            content="Front-end Developer specialized in React JS and React Native with typescript"
+          />
+          <meta
+            property="og:image"
+            content="https://www.wendson.com.br/logo.png"
+          />
+          <meta
+            property="og:image:secure_url"
+            itemProp="image"
+            content="https://www.wendson.com.br/logo.png"
+          />
+          <meta property="og:image:width" content="93" />
+          <meta property="og:image:height" content="48" />
+          <meta property="twitter:card" content="summary_large_image" />
+          <meta property="twitter:url" content="https://www.wendson.com.br/" />
+          <meta
+            property="twitter:title"
+            content="Wendson Sousa - Front-end Developer"
+          />
+          <meta
+            property="twitter:description"
+            content="Front-end Developer specialized in React JS and React Native with typescript"
+          />
+          <meta
+            property="twitter:image"
+            content="https://www.wendson.com.br/logo.png"
+          />
+          ;
+          <meta property="twitter:image:width" content="93" />
+          <meta property="twitter:image:height" content="48" />
+        </Head>
 
-      {children}
+        {children}
 
-      <GlobalStyle />
-    </ContextThemeProvider>
+        <GlobalStyle />
+      </ContextThemeProvider>
+    </div>
   );
 }
