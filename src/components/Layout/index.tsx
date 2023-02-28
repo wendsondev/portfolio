792cde3136f8cbb53da6ca49b5bb3e30075ba53d
wendson13 <wendson.dev@gmail.com>
2023-02-28 18:00:34 -0300

    feat: adding config for responsive icons and shared image

--- src/components/Layout/index.tsx
+++ src/components/Layout/index.tsx
@@ -36,12 +36,80 @@ export function Layout({ children }: LayoutProps) {
             href="https://www.wendsondev.com/en"
             hrefLang="x-default"
           />
-          <link rel="shortcut icon" href="favicon.png" type="image/png" />
+          <link
+            rel="icon"
+            sizes="16x16"
+            href="favicon/favicon-16x16.png"
+            type="image/png"
+          />
+          <link
+            rel="icon"
+            sizes="32x32"
+            href="favicon/favicon-32x32.png"
+            type="image/png"
+          />
+          <link
+            rel="icon"
+            sizes="96x96"
+            href="favicon/favicon-96x96.png"
+            type="image/png"
+          />
+          <link
+            rel="icon"
+            sizes="192x192"
+            href="favicon/favicon-192x192.png"
+            type="image/png"
+          />
+          <link
+            rel="apple-touch-icon"
+            sizes="57x57"
+            href="favicon/apple-icon-57x57.png"
+          />
+          <link
+            rel="apple-touch-icon"
+            sizes="60x60"
+            href="favicon/apple-icon-60x60.png"
+          />
+          <link
+            rel="apple-touch-icon"
+            sizes="72x72"
+            href="favicon/apple-icon-72x72.png"
+          />
+          <link
+            rel="apple-touch-icon"
+            sizes="76x76"
+            href="favicon/apple-icon-76x76.png"
+          />
+          <link
+            rel="apple-touch-icon"
+            sizes="114x114"
+            href="favicon/apple-icon-114x114.png"
+          />
+          <link
+            rel="apple-touch-icon"
+            sizes="120x120"
+            href="favicon/apple-icon-120x120.png"
+          />
+          <link
+            rel="apple-touch-icon"
+            sizes="144x144"
+            href="favicon/apple-icon-144x144.png"
+          />
+          <link
+            rel="apple-touch-icon"
+            sizes="152x152"
+            href="favicon/apple-icon-152x152.png"
+          />
+          <link
+            rel="apple-touch-icon"
+            sizes="180x180"
+            href="favicon/apple-icon-180x180.png"
+          />
+
           <meta name="title" content="Wendson Sousa - Front-end Developer" />
           <meta
             name="description"
-            content="Front-end Developer specialized in React JS and React Native with typescript
-  "
+            content="Front-end Developer specialized in React JS and React Native with typescript"
           />
           <meta property="og:type" content="website" />
           <meta property="og:url" content="https://www.wendsondev.com/" />
@@ -55,15 +123,13 @@ export function Layout({ children }: LayoutProps) {
           />
           <meta
             property="og:image"
-            content="https://www.wendsondev.com/logo.png"
+            content="https://www.wendsondev.com/share-image.jpg"
           />
           <meta
             property="og:image:secure_url"
             itemProp="image"
-            content="https://www.wendsondev.com/logo.png"
+            content="https://www.wendsondev.com/share-image.jpg"
           />
-          <meta property="og:image:width" content="93" />
-          <meta property="og:image:height" content="48" />
           <meta property="twitter:card" content="summary_large_image" />
           <meta property="twitter:url" content="https://www.wendsondev.com/" />
           <meta
@@ -76,10 +142,8 @@ export function Layout({ children }: LayoutProps) {
           />
           <meta
             property="twitter:image"
-            content="https://www.wendsondev.com/logo.png"
+            content="https://www.wendsondev.com/share-image.jpg"
           />
-          <meta property="twitter:image:width" content="93" />
-          <meta property="twitter:image:height" content="48" />
         </Head>
 
         <Header />
--- src/styles/global.ts
+++ src/styles/global.ts
@@ -14,5 +14,6 @@ export const GlobalStyle = createGlobalStyle`
   body {
     width: 100%;
     height: 100vh;
+    background: ${({ theme }) => theme.colors.background.primary};
   }
 `;
